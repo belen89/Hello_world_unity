23ae2c56aaf343c58c0a03692c0ef424a5205c9a
Belén Gudiño <bgudino@brandigital.com>
2015-06-24 16:40:41 -0300

    Ultima ok

---
+++ New Unity Project/Assets/TweetBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TweetBehaviourScript : MonoBehaviour {
+
+	int count=0;
+	bool showit=true;
+	GameObject Pack_fiesta;
+	bool pack=false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per 10 frames
+	void FixedUpdate ()
+	{
+		Pack_fiesta = GameObject.Find ("Pack_fiesta");
+		pack = Pack_fiesta.GetComponent<Renderer> ().enabled;
+		if (pack) {
+			Time.fixedDeltaTime = 5F;
+			if (showit) {
+				//gameObject.GetComponent<Renderer>().material.color=Color.black;
+				gameObject.GetComponent<Renderer> ().enabled = false;
+				showit = false;
+			} else {
+				//gameObject.GetComponent<Renderer>().material.color=Color.white;
+				gameObject.GetComponent<Renderer> ().enabled = true;
+				showit = true;
+			}
+			Debug.Log (gameObject.name);
+		}
+		
+	}
+	
+	
+	void Update ()
+	{
+		//Debug.Log("Update time :" + Time.deltaTime);
+	}
+}
