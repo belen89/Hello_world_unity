b81a0de3abbb82cf8042c646bc74ddd8011d84f8
Belén Gudiño <belen89@gmail.com>
2015-06-25 01:17:59 -0300

    Formulario
    Aparece el formulario y vuelve a la camara de AR

---
+++ New Unity Project/Assets/discoverBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class discoverBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("Abrir formulario");
+
+	}
+}
